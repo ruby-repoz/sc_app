1ed4c35a69eb7d6fb945be410d3a6c6d3e33c1dd
VikShah <my.ruby.courses@gmail.com>
2012-12-26 14:02:05 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScApp::Application.config.secret_token = '5eb4e7ce8548047df443268687ff3c4e309fc1079345a7c175495d3adf953d63d4b6f3a716ef443d17b425ab090ff9a4f9d72224bcf9f7da9c636655062fa50d'
